1638fd52440801ac9e81ba6bd45b43a511b0d74f
Yang Lin <yanglin5689446@gmail.com>
2019-05-15 15:51:29 +0800

    Add reload button(for debugging)

--- src/pages/Done/index.js
+++ src/pages/Done/index.js
@@ -2,11 +2,14 @@
 import React from 'react'
 import {
   withStyles,
+  Fab,
 } from '@material-ui/core'
+import RefreshIcon from '@material-ui/icons/Refresh'
 
 import TimeSlice from 'components/Mail/TimeSlice'
 
 import useProcessedMails from 'utils/hooks/processed_mails'
+import useGmailAPI from 'utils/hooks/gmail_api'
 
 const styles = () => ({
   container: {
@@ -15,23 +18,46 @@ const styles = () => ({
     marginLeft: 'auto',
     marginRight: 'auto',
   },
+  reloadButtonContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  refreshIcon: {
+    margin: 2,
+  },
 })
 
 const Done = ({ classes }) => {
   const processed = useProcessedMails({ excludes: ['INBOX', 'TRASH', 'SPAM'] })
+  const { loadMails } = useGmailAPI()
 
   return (
     <div className={classes.container}>
-      {
-        processed
-          ? processed.map(clusters => (
-            <TimeSlice
-              key={clusters.label}
-              clusters={clusters}
-            />
-          ))
-          : 'Loading...'
-      }
+      <div className={classes.reloadButtonContainer}>
+        <Fab
+          variant='extended'
+          size='small'
+          color='primary'
+          onClick={loadMails}
+          className={classes.reloadButton}
+        >
+          <RefreshIcon className={classes.refreshIcon} />
+          Reload
+        </Fab>
+      </div>
+      <div>
+        {
+          processed
+            ? processed.map(clusters => (
+              <TimeSlice
+                key={clusters.label}
+                clusters={clusters}
+              />
+            ))
+            : 'Loading...'
+        }
+      </div>
+
     </div>
   )
 }
