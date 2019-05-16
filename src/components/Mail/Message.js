6f517ad79e40bbbb60d02628b8dcb3e1f708063a
Yang Lin <yanglin5689446@gmail.com>
2019-05-16 16:37:48 +0800

    Add mail stylesheet scope

--- src/components/Mail/Message.js
+++ src/components/Mail/Message.js
@@ -1,6 +1,6 @@
 
 import React, {
-  useState, useEffect, useContext, useMemo,
+  useState, useEffect, useContext, useMemo, useRef,
 } from 'react'
 import {
   withStyles,
@@ -9,10 +9,13 @@ import {
   Avatar,
   colors,
 } from '@material-ui/core'
+import uuid from 'uuid/v1'
+
 import DeleteIcon from '@material-ui/icons/Delete'
 
 import MailsContext from 'context/mails'
 import useGmailAPI from 'utils/hooks/gmail_api'
+import cssParser from 'css'
 
 const styles = () => ({
   root: {
@@ -44,8 +47,7 @@ const styles = () => ({
   },
   body: {
     padding: 4,
-    width: '100%',
-    maxWidth: '100%',
+    width: 'calc(100% - 32px - 12px * 2)',
   },
   snippet: {
     whiteSpace: 'nowrap',
@@ -56,7 +58,7 @@ const styles = () => ({
   },
 })
 
-const processHTMLContent = (raw) => {
+const processHTMLContent = (scope, raw) => {
   const parser = new DOMParser()
   const serializer = new XMLSerializer()
   const doc = parser.parseFromString(raw, 'text/html')
@@ -66,6 +68,30 @@ const processHTMLContent = (raw) => {
   // add target='_blank' to all areas
   const areas = Array.from(doc.getElementsByTagName('area'))
   areas.forEach(link => link.setAttribute('target', '_blank'))
+
+  const stylesheets = Array.from(doc.getElementsByTagName('style'))
+  const stripAndAddScope = selectors => selectors
+    .filter(selector => selector.toLowerCase !== 'body')
+    .map(selector => `#${scope} ${selector}`)
+  stylesheets.forEach((stylesheet) => {
+    try {
+      const css = cssParser.parse(stylesheet.innerText)
+      Object.values(css.stylesheet.rules)
+        .forEach((rule) => {
+          if (rule.type === 'rule') {
+            rule.selectors = stripAndAddScope(rule.selectors) // eslint-disable-line
+          } else if (rule.type === 'media') {
+            rule.rules.forEach((r) => {
+              r.selectors = stripAndAddScope(r.selectors)  // eslint-disable-line
+            })
+          }
+        })
+      stylesheet.innerText = cssParser.stringify(css, { compress: true })  // eslint-disable-line
+    } catch (e) {
+      stylesheet.innerText = ''  // eslint-disable-line
+    }
+  })
+
   return {
     content: serializer.serializeToString(doc),
   }
@@ -77,13 +103,14 @@ const Message = ({
   const { removeMessageLabel } = useContext(MailsContext)
   const { modifyMessage, trashMessage } = useGmailAPI()
   const [expanded, setExpanded] = useState(initialExpand)
+  const scope = useRef(uuid())
   useEffect(() => {
     if (expanded && unread) {
       modifyMessage({ id, remove: ['UNREAD'] })
       removeMessageLabel({ threadId, id, label: 'UNREAD' })
     }
   }, [unread, expanded])
-  const cooked = useMemo(() => processHTMLContent(content), [content])
+  const cooked = useMemo(() => processHTMLContent(scope.current, content), [scope, content])
   return (
     <Card
       onClick={() => setExpanded(exp => !exp)}
@@ -111,7 +138,7 @@ const Message = ({
           </div>
           {
             expanded
-              ? <div dangerouslySetInnerHTML={{ __html: cooked.content }} />
+              ? <div id={scope.current} dangerouslySetInnerHTML={{ __html: cooked.content }} />
               : <div className={classes.snippet}>{ snippet }</div>
           }
         </div>
